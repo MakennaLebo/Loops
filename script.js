2fb40eefa08d4d39c6666d8889ba2ed4b296cc09
MakennaLebo <kennalebo@hotmail.com>
2022-07-13 18:18:29 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+
+ for(let d =1; d>=10; d--){
+    console.log(d);
+ }
+
+ let x = 1;
+ while (x<=10){
+     console.log(x);
+     x++;
+ }
+
+ let c = 10;
+ while (c>=1){
+     console.log(c);
+     c--;
+ }
+
+ let count = 0;
+ do{
+    console.log(count);
+    count++;
+ }while (count <= 10)
+
+ let downcount = 10;
+ do{
+    console.log(downcount);
+    downcount--;
+ }while (downcount >=1);
+
+ 
